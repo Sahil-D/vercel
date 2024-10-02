f57e1011a845276a2f76e6aa95634c67a8ff1d30
Sahil-D <sunnydawara00@gmail.com>
2024-10-02 23:01:39 +0530

    added log pipeline as jugaadu API

--- api-server/index.js
+++ api-server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { generateSlug } = require('random-word-slugs');
 const { ECSClient, RunTaskCommand } = require('@aws-sdk/client-ecs');
+const {
+  CloudWatchLogsClient,
+  GetLogEventsCommand,
+} = require('@aws-sdk/client-cloudwatch-logs');
 const cors = require('cors');
 const { z } = require('zod');
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient, DeploymentStatus } = require('@prisma/client');
 
 const app = express();
 
@@ -17,13 +21,18 @@ const AWS_ECS_SECRET_ACCESS_KEY = process.env.AWS_ECS_SECRET_ACCESS_KEY;
 // cluster config
 const AWS_ECS_CLUSTER = process.env.AWS_ECS_CLUSTER;
 const AWS_ECS_TASK = process.env.AWS_ECS_TASK;
+const AWS_ECS_REGION = process.env.AWS_ECS_REGION;
 
 const ecsClient = new ECSClient({
-  region: 'ap-south-1',
+  region: AWS_ECS_REGION,
   credentials: {
     accessKeyId: AWS_ECS_ACCESS_KEY_ID,
     secretAccessKey: AWS_ECS_SECRET_ACCESS_KEY,
   },
+});
+
+const cloudWatchLogsClient = new CloudWatchLogsClient({
+  region: AWS_ECS_REGION,
 });
 
 const config = {
@@ -49,7 +58,18 @@ app.post('/project', async (req, res) => {
 
   const { name, gitURL } = parsedBody.data;
 
-  const projectSlug = generateSlug();
+  let projectSlug = generateSlug();
+
+  let previousProject = await prisma.project.findFirst({
+    where: { subDomain: projectSlug + '-' + name },
+  });
+
+  while (previousProject) {
+    projectSlug = generateSlug();
+    previousProject = await prisma.project.findFirst({
+      where: { subDomain: projectSlug + '-' + name },
+    });
+  }
 
   const project = await prisma.project.create({
     data: {
@@ -68,6 +88,26 @@ app.post('/deploy', async (req, res) => {
   const project = await prisma.project.findUnique({ where: { id: projectId } });
 
   if (!project) return res.status(404).json({ error: 'Project Not Found' });
+
+  // check all deployments if any one has projectId same to it, it will mean that project is deployed
+  const previousDeployment = await prisma.deployment.findFirst({
+    where: {
+      projectId: projectId,
+    },
+  });
+
+  if (previousDeployment) {
+    if (previousDeployment.status != DeploymentStatus.READY) {
+      return res.status(409).json({
+        error:
+          'Previous Deployment exist with status : ' +
+          previousDeployment.status,
+      });
+    } else {
+      console.log('Detected previous failed deployment, Trying again...');
+    }
+  }
+
   const projectSlug = project.subDomain;
 
   // Spin the container
@@ -117,6 +157,39 @@ app.post('/deploy', async (req, res) => {
       url: `http://${projectSlug}.localhost:${PROXY_SERVER_PORT}`,
     },
   });
+});
+
+app.get('/logs', async (req, res) => {
+  const { deploymentId } = req.body;
+
+  const deployment = await prisma.deployment.findUnique({
+    where: {
+      id: deploymentId,
+    },
+  });
+
+  if (!deployment) {
+    return res.status(404).json({ error: 'No such deployment exists' });
+  }
+
+  try {
+    const LOG_GROUP_NAME = process.env.AWS_ECS_LOG_GROUP_NAME;
+    const LOG_STREAM_NAME = process.env.AWS_ECS_LOG_STREAM_NAME + deploymentId;
+
+    const getLogEventsCommand = new GetLogEventsCommand({
+      logGroupName: LOG_GROUP_NAME,
+      logStreamName: LOG_STREAM_NAME,
+      limit: parseInt(process.env.AWS_ECS_LOG_LIMIT, 10),
+    });
+
+    const response = await cloudWatchLogsClient.send(getLogEventsCommand);
+    const logList = response.events.map((e) => e.message);
+    // This is by default sorted
+
+    return res.json({ logs: logList });
+  } catch (e) {
+    return res.status(500).json({ error: 'Error fetching logs: ' + e });
+  }
 });
 
 app.listen(PORT, () => console.log(`API server running on PORT : ${PORT} `));
